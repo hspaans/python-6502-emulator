45ed538db6372c2306c2be2b9c796a9d031b19ac
Hans Spaans <hans@dailystuff.nl>
2022-01-03 21:31:33 +0000

    Fix layout for assert

--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -10,4 +10,16 @@ def test_cpu_reset() -> None:
     memory = m6502.Memory()
     cpu = m6502.Processor(memory)
     cpu.reset()
-    assert (cpu.program_counter, cpu.stack_pointer, cpu.flag_b, cpu.flag_d, cpu.flag_i) == (0xFCE2, 0x01FD, True, False, True)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i
+    ) == (
+        0xFCE2,
+        0x01FD,
+        True,
+        False,
+        True
+    )
