5c4632a3074b9cebad3b6b964c0f7968268e44ef
Hans Spaans <hans@dailystuff.nl>
2022-01-16 14:43:08 +0100

    Adding memory operations (#6)
    * Add basic functions to read from or write to memory
    
    * Updating descriptions
    
    * Make the code platform independent
    
    * Correcting test_cpu_reset()
    
    * Remove whitespaces

--- m6502/processor.py
+++ m6502/processor.py
@@ -1,4 +1,6 @@
 """Emulation of the MOT-6502 Processor."""
+import sys
+
 import m6502
 
 
@@ -36,6 +38,73 @@ class Processor:
         self.program_counter = 0xFCE2  # Hardcoded start vector post-reset
         self.stack_pointer   = 0x01FD  # Hardcoded stack pointer post-reset
         self.cycles          = 0
+
         self.flag_i = True
         self.flag_d = False
         self.flag_b = True
+
+    def fetch_byte(self) -> int:
+        """Fetch a byte from memory.
+
+        :param address: The address to read from
+        :return: int
+        """
+        data = self.read_byte(self.program_counter)
+        self.program_counter += 1
+        return data
+
+    def fetch_word(self) -> int:
+        """Fetch a word from memory.
+
+        :param address: The address to read from
+        :return: int
+        """
+        data = self.read_word(self.program_counter)
+        self.program_counter += 2
+        return data
+
+    def read_byte(self, address: int) -> int:
+        """Read a byte from memory.
+
+        :param address: The address to read from
+        :return: int
+        """
+        data = self.memory[address]
+        self.cycles += 1
+        return data
+
+    def read_word(self, address: int) -> int:
+        """Read a word from memory.
+
+        :param address: The address to read from
+        :return: int
+        """
+        if sys.byteorder == "little":
+            data = self.read_byte(address) | (self.read_byte(address + 1) << 8)
+        else:
+            data = (self.read_byte(address) << 8) | self.read_byte(address + 1)
+        return data
+
+    def write_byte(self, address: int, value: int) -> None:
+        """Write a byte to memory.
+
+        :param address: The address to write to
+        :param value: The value to write
+        :return: None
+        """
+        self.memory[address] = value
+        self.cycles += 1
+
+    def write_word(self, address: int, value: int) -> None:
+        """Split a word to two bytes and write to memory.
+
+        :param address: The address to write to
+        :param value: The value to write
+        :return: None
+        """
+        if sys.byteorder == "little":
+            self.write_byte(address, value & 0xFF)
+            self.write_byte(address + 1, (value >> 8) & 0xFF)
+        else:
+            self.write_byte(address, (value >> 8) & 0xFF)
+            self.write_byte(address + 1, value & 0xFF)
--- tests/test_cpu.py
+++ tests/test_cpu.py
@@ -16,12 +16,198 @@ def test_cpu_reset() -> None:
         cpu.cycles,
         cpu.flag_b,
         cpu.flag_d,
-        cpu.flag_i
-    ) == (
-        0xFCE2,
-        0x01FD,
-        0,
-        True,
-        False,
-        True
-    )
+        cpu.flag_i,
+    ) == (0xFCE2, 0x01FD, 0, True, False, True)
+
+
+def test_cpu_read_byte() -> None:
+    """Verify CPU can read a byte from memory.
+
+    The cost of the read operation is 1 cycle, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    memory[0x0001] = 0xA5
+    value = cpu.read_byte(0x0001)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE2, 0x01FD, 1, True, False, True, 0xA5)
+
+
+def test_cpu_read_word() -> None:
+    """Verify CPU can read a word from memory.
+
+    The cost of the read operation is 2 cycles, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    memory[0x0001] = 0xA5
+    memory[0x0002] = 0x5A
+    value = cpu.read_word(0x0001)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE2, 0x01FD, 2, True, False, True, 0x5AA5)
+
+
+def test_cpu_write_byte() -> None:
+    """Verify CPU can write a byte to memory.
+
+    The cost of the write operation is 1 cycle, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    cpu.write_byte(0x0001, 0xA5)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        memory[0x0001],
+    ) == (0xFCE2, 0x01FD, 1, True, False, True, 0xA5)
+
+
+def test_cpu_write_word() -> None:
+    """Verify CPU can write a byte to memory.
+
+    The cost of the write operation is 1 cycle, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    cpu.write_word(0x0001, 0x5AA5)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        memory[0x0001],
+        memory[0x0002],
+    ) == (0xFCE2, 0x01FD, 2, True, False, True, 0xA5, 0x5A)
+
+
+def test_cpu_read_write_byte() -> None:
+    """Verify CPU can read and write a byte from memory.
+
+    The cost of the read operation is 1 cycle, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    cpu.write_byte(0x0001, 0xA5)
+    value = cpu.read_byte(0x0001)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE2, 0x01FD, 2, True, False, True, 0xA5)
+
+
+def test_cpu_read_write_word() -> None:
+    """Verify CPU can read and write a byte from memory.
+
+    The cost of the read operation is 1 cycle, and the state of the CPU is
+    not changed.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    cpu.write_word(0x0001, 0x5AA5)
+    value = cpu.read_word(0x0001)
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE2, 0x01FD, 4, True, False, True, 0x5AA5)
+
+
+def test_cpu_fetch_byte() -> None:
+    """Verify CPU can fetch a byte from memory.
+
+    The cost of the fetch operation is 1 cycle, and increases the program
+    counter by 1. The state of the CPU is not changed further.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    memory[0xFCE2] = 0xA5
+    value = cpu.fetch_byte()
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE3, 0x01FD, 1, True, False, True, 0xA5)
+
+
+def test_cpu_fetch_word() -> None:
+    """Verify CPU can fetch a word from memory.
+
+    The cost of the fetch operation is 2 cycle, and increases the program
+    counter by 2. The state of the CPU is not changed further.
+
+    :return: None
+    """
+    memory = m6502.Memory()
+    cpu = m6502.Processor(memory)
+    cpu.reset()
+    memory[0xFCE2] = 0xA5
+    memory[0xFCE3] = 0x5A
+    value = cpu.fetch_word()
+    assert (
+        cpu.program_counter,
+        cpu.stack_pointer,
+        cpu.cycles,
+        cpu.flag_b,
+        cpu.flag_d,
+        cpu.flag_i,
+        value,
+    ) == (0xFCE4, 0x01FD, 2, True, False, True, 0x5AA5)
